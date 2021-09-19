a668f4d7ea2ad62f23bcacb81fb41e7d2a01ed93
greenitop <fc53457@alunos.fc.ul.pt>
2021-09-19 18:39:29 +0100

    makefile para teste data

--- Etapa 1/source/data.c
+++ Etapa 1/source/data.c
@@ -3,24 +3,34 @@
 /* Função que cria um novo elemento de dados data_t e reserva a memória
  * necessária, especificada pelo parâmetro size 
  */
-struct data_t *data_create(int size);
+struct data_t *data_create(int size){
+    
+}
 
 /* Função idêntica à anterior, mas que inicializa os dados de acordo com
  * o parâmetro data.
  */
-struct data_t *data_create2(int size, void *data); 
+struct data_t *data_create2(int size, void *data){
+
+}
 
 /* Função que elimina um bloco de dados, apontado pelo parâmetro data,
  * libertando toda a memória por ele ocupada.
  */
-void data_destroy(struct data_t *data);
+void data_destroy(struct data_t *data){
+
+}
 
 /* Função que duplica uma estrutura data_t, reservando a memória
  * necessária para a nova estrutura.
  */
-struct data_t *data_dup(struct data_t *data);
+struct data_t *data_dup(struct data_t *data){
+
+}
 
 /* Função que substitui o conteúdo de um elemento de dados data_t.
 *  Deve assegurar que destroi o conteúdo antigo do mesmo.
 */
-void data_replace(struct data_t *data, int new_size, void *new_data);
+void data_replace(struct data_t *data, int new_size, void *new_data){
+
+}

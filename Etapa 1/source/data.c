806652842956547d2c25362e5eb23fb27c132c62
greenitop <fc53457@alunos.fc.ul.pt>
2021-09-20 17:51:45 +0100

    data 0 leaks

--- Etapa 1/source/data.c
+++ Etapa 1/source/data.c
@@ -33,20 +33,24 @@ struct data_t *data_create(int size)
  */
 struct data_t *data_create2(int size, void *data)
 {
-    struct data_t *d = data_create(size);
-    if (d == NULL || size <= 0)
+    if (data == NULL || size <= 0)
+    {
+        return NULL;
+    }
+    struct data_t *d = (struct data_t *)malloc(sizeof(struct data_t));
+    if (d == NULL)
     {
         free(d);
         return NULL;
     }
-    if (data == NULL)
+    d->datasize = size;
+    d->data = data;
+    if (d->data == NULL)
     {
         free(d->data);
         free(d);
         return NULL;
     }
-
-    d->data = data;
     return d;
 }
 
@@ -58,6 +62,10 @@ void data_destroy(struct data_t *data)
     if (data != NULL)
     {
         free(data->data);
+        free(data);
+    }
+    else
+    {
         free(data);
     }
 }

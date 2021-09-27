8bd5066d9a7615964ff8c992b6162cb6ef4e1c65
Andre Seixas <andrefil.seixas@gmail.com>
2021-09-27 16:39:59 +0100

    Corrigir erros do list.c

---
+++ Projeto/Etapa 1/include/list-private.h
@@ -0,0 +1,16 @@
+#ifndef _LIST_PRIVATE_H
+#define _LIST_PRIVATE_H
+
+#include "list.h"
+
+struct node_t {
+    struct entry_t *value;
+    struct node_t *next;
+};
+
+struct list_t {
+    struct node_t *head;
+    int length;
+};
+
+#endif

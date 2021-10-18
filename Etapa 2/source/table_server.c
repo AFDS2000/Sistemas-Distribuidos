77c2cb6e0b3e01c995a5f3c7de0d656ba42a4c44
André Seixas <fc53870@alunos.fc.ul.pt>
2021-10-18 19:25:35 +0100

    Ficheiro base atualizados

--- Etapa 2/source/table_server.c
+++ Etapa 2/source/table_server.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #include "table_skel.h"
-#include "table-private.h"
+#include "network_server.h"
 
 int main(int argc, char const *argv[])
 {
@@ -11,13 +11,20 @@ int main(int argc, char const *argv[])
         printf("Execução do programa: ./<path_to_exe>/table.server <port> <n_lists>\n");
         return -1;
     }
-
+    int err;
     int port = atoi(argv[1]);
     int n_lists = atoi(argv[2]);
 
     printf("Port: %d\nN-lists: %d\n", port, n_lists);
 
+    //int socket_listenning = network_server_init(port);
+
     //inicializar a table
-    table_skel_init(n_lists);
+    err = table_skel_init(n_lists);
+    if (err != 0)
+        return err;
+    
+    //destroir a table
+    table_skel_destroy();
     return 0;
 }
